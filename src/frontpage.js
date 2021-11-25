c85143e99db1e2e2e7e6f53a7cd36a38f406db00
kiBROm <danu@localhost.localdomain>
2021-11-25 10:45:32 -0500

    Setup Comment section
    added scroll suggestion

--- src/frontpage.js
+++ src/frontpage.js
@@ -10,20 +10,20 @@ import img5 from "./ART/5.jpg";
 import img6 from "./ART/6.jpg";
 
 
-import {BrowserRouter as Brouter,Routes,Route,Link } from "react-router-dom";
+import { BrowserRouter as Brouter, Routes, Route, Link } from "react-router-dom";
 
 export default function Frontpage() {
-  
+
     return (<Brouter>
-      <Routes>
-        <Route path="/" element={<ImageList />} />
-        <Route path=":slug" element={<ViewImg />} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={<ImageList />} />
+            <Route path=":slug" element={<ViewImg />} />
+        </Routes>
     </Brouter>)
 
 }
 
-function ImageList(){
+function ImageList() {
 
     const [imgone_boxstatus, set_imgone_boxstatus] = useState({ display: "none" });
     const [imgtwo_boxstatus, set_imgtwo_boxstatus] = useState({ display: "none" });
@@ -32,75 +32,124 @@ function ImageList(){
     const [imgfive_boxstatus, set_imgfive_boxstatus] = useState({ display: "none" });
     const [imgsix_boxstatus, set_imgsix_boxstatus] = useState({ display: "none" });
 
+    arts.img1.boxstatus = [imgone_boxstatus, set_imgone_boxstatus];
+    arts.img2.boxstatus = [imgtwo_boxstatus, set_imgtwo_boxstatus];
+    arts.img3.boxstatus = [imgthree_boxstatus, set_imgthree_boxstatus];
+    arts.img4.boxstatus = [imgfour_boxstatus, set_imgfour_boxstatus];
+    arts.img5.boxstatus = [imgfive_boxstatus, set_imgfive_boxstatus];
+    arts.img6.boxstatus = [imgsix_boxstatus, set_imgsix_boxstatus];
 
- /*   const [imgone_likebtn_status, set_imgone_likebtn_status] = useState("white");
-    const [imgtwo_likebtn_status, set_imgtwo_likebtn_status] = useState("white");
-    const [imgthree_likebtn_status, set_imgthree_likebtn_status] = useState("white");
-    const [imgfour_likebtn_status, set_imgfour_likebtn_status] = useState("white");
-    const [imgfive_likebtn_status, set_imgfive_likebtn_status] = useState("white");
-    const [imgsix_likebtn_status, set_imgsix_likebtn_status] = useState("white");  */
 
-  
-
-            arts.img1.boxstatus = [imgone_boxstatus , set_imgone_boxstatus ];
-            arts.img2.boxstatus =  [imgtwo_boxstatus , set_imgtwo_boxstatus ];
-            arts.img3.boxstatus =  [imgthree_boxstatus , set_imgthree_boxstatus ] ;
-            arts.img4.boxstatus =  [imgfour_boxstatus , set_imgfour_boxstatus ];
-            arts.img5.boxstatus =  [imgfive_boxstatus , set_imgfive_boxstatus ];
-            arts.img6.boxstatus =  [imgsix_boxstatus , set_imgsix_boxstatus ];
 
 
 
     return (
         <section id="main_container" style={maincontainer_style} >
 
-           <ul>
-            {Object.entries(arts).map(([slug , {title,artist,price,img,boxstatus}])=> 
-            <li key={slug}>
+            <ul>
+                {Object.entries(arts).map(([slug, { title, artist, price, img, boxstatus }]) =>
+                    <li key={slug}>
 
-                <Link to={`/${slug}`}>
-        <figure className="img_textover" style={img_textover}>
+                        <Link to={`/${slug}`}>
+                            <figure className="img_textover" style={img_textover}>
 
-                  <img style={frontimg_style} src={img} alt="" />
-                  <figcaption  style={figcaption_style_visible} className="figcaption"
-                  onMouseOver={() => {
-                      boxstatus[1]({
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "flex-end",
-                          background: "-moz-linear-gradient(rgba(0,0,0,0) 55%, black)",
-                          height: "30vh"
-                      })
-                  }}
-                  onMouseOut={()=> { boxstatus[1]({display:"none"})}}
-              >
+                                <img style={frontimg_style} src={img} alt="" />
+                                <figcaption style={figcaption_style_visible} className="figcaption"
+                                    onMouseOver={() => {
+                                        boxstatus[1]({
+                                            display: "flex",
+                                            justifyContent: "space-between",
+                                            alignItems: "flex-end",
+                                            background: "-moz-linear-gradient(rgba(0,0,0,0) 55%, black)",
+                                            height: "30vh"
+                                        })
+                                    }}
+                                    onMouseOut={() => { boxstatus[1]({ display: "none" }) }}
+                                >
 
-              <section style={boxstatus[0]}>
-                <p style={figcaption_para}>{`${title} by ${artist}`}</p>
-            </section>
-              </figcaption>
-                  </figure>
-                </Link>
-            </li>
-           
-            ) }
-           </ul>
+                                    <section style={boxstatus[0]}>
+                                        <p style={figcaption_para}>{`${title} by ${artist}`}</p>
+                                    </section>
+                                </figcaption>
+                            </figure>
+                        </Link>
+                    </li>
+
+                )}
+            </ul>
         </section>
     )
 }
 
 function ViewImg() {
-    const {slug} = useParams();
+    const { slug } = useParams();
+
+    const [showcomments, setshowcomments] = useState(false);
 
     const art = arts[slug];
-    return (<>           
+    return (<>
         <img style={viewimg_imgstyle} src={art.img} alt="" />
+        <section style={halfpagecontainer_style}>
+        <div style={arttitlecontainer_style}><p style={arttitlepara_style}>{art.title} - {art.artist}</p></div>
+       
+        {showcomments ? <CommentsSection commentFunction={setshowcomments}/> : <SuggestionImgs artobject={art} commentFunction={setshowcomments}/>}
+        </section>
     </>);
 }
 
+function CommentsSection(prop) {
+    return(<div style={commentcancle_containerstyle}> 
+        <p style={commentcancle_parastyle}>Comments</p>
+    <button style={canclecomment_style} onClick={() => { prop.commentFunction(false)}}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="2.3em" height="2.3em" viewBox="0 0 16 16"><g fill="currentColor">
+    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8L4.646 5.354a.5.5 0 0 1 0-.708z"/></g></svg>
+    </button></div>);
+}
+function SuggestionImgs(prop) {
+    return (<>
+        <div style={pricelikebtn_container}>
+        <p style={pricepara_style}>Price : {prop.artobject.price}</p>
+
+        <div style={likebtn_container}>
+            <button style={likebutton_style} onClick
+                onMouseOver={(e) => { e.target.style.opacity = ".8"; }} onMouseOut={(e) => { e.target.style.opacity = "1"; }}>
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    width="1.5em" height="1.5em" viewBox="0 0 48 48">
+                    <path d="M15 8C8.925 8 4 12.925 4 19c0 11 13 21 20 23.326C31 40 44 30 44 19c0-6.075-4.925-11-11-11c-3.72 0-7.01 1.847-9 4.674A10.987 10.987 0 0 0 15 8z" fill={prop.artobject.likestatus[0]} stroke="#000" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" /></svg>
+            </button>
+            <p style={likecountpar_style}>{prop.artobject.likestatus[3]}</p>
+        </div>
+    </div>
+    <div style={accountbuy_container_style}>
+        <p style={accountpar_style}>{prop.artobject.account}</p>
+        <button style={buybtn_style}  onMouseOver={(e) => { e.target.style.opacity = ".8"; }} onMouseOut={(e) => { e.target.style.opacity = "1"; }}>Buy</button>
+    </div>
+    <div style={commentbtns_container}>
+        <button style={commentsbtn_style} onClick={() => { prop.commentFunction(true); }}>comments  {prop.artobject.commentsSize}</button>
+        <button style={commentdropdownbtn_style} onClick={() => { prop.commentFunction(true); }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16">
+                <g fill="currentColor"><path d="M2 5.56L2.413 5h11.194l.393.54L8.373 11h-.827L2 5.56z" /></g></svg></button>
+    </div>
+        <section id="main_container" style={suggestioncontainer_style} >
+
+    <ul>
+        {Object.entries(arts).map(([slug, { title, artist, price, img, boxstatus }]) =>
+            <li key={slug}>
+                        <img style={frontimg_style} src={img} alt="" />        
+            </li>
+
+        )}
+    </ul>
+</section></>);
+}
+
+const halfpagecontainer_style = {
+    height: "48vh",
+    overflowY : "scroll",
+}
 const viewimg_imgstyle = {
     width: "100%",
-    height: "38vh",
+    height: "45vh",
     marginTop: "7vh",
 }
 const maincontainer_style = {
@@ -136,14 +185,136 @@ const figcaption_para = {
     textOverflow: "ellipsis",
     paddingLeft: "2%",
 }
+const arttitlecontainer_style = {
+    margin: "0",
+    borderBottom: "1px solid",
+}
+const arttitlepara_style = {
+    margin: "0",
+    fontFamily: "cursive",
+    marginLeft: "2%",
+    fontWeight: "900",
+    textAlign: "center",
+
+}
+const pricelikebtn_container = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "7vh",
+    margin: "0 auto",
+    borderBottom: "1px solid #e1dbdb",
+
+}
+const pricepara_style = {
+    paddingLeft: "5%",
+    fontSize: ".9rem",
+    fontWeight: "900",
+    color: "purple",
+    width: "60%",
+
+}
+const likebtn_container = {
+    display: "flex",
+    flexDirection: "column",
+    width: "20%",
+    margin: "0 auto",
+}
+const likebutton_style = {
+    backgroundColor: "rgba(0,0,0,0)",
+    marginRight: "10%",
+    border: "none",
+    paddingLeft: "10%",
+}
+const canclecomment_style = {
+    backgroundColor: "rgba(0,0,0,0)",
+    border: "none",
+    paddingTop : ".3rem",
+    marginRight: "4%",
+}
+const likecountpar_style = {
+    fontSize: ".7rem",
+    fontWeight: "900",
+    margin: "0",
+    alignSelf: "center",
+}
+const buybtn_style = {
+    padding: "0rem 1.6rem",
+    backgroundColor: "#ff6000",
+    color: "black",
+    fontWeight: "700",
+    textTransform: "uppercase",
+    border: "none",
+    height: "2.1rem",
+    marginTop: ".5rem",
+    marginRight: "1.3rem",
+    opacity: "1",
+
+}
+const accountbuy_container_style = {
+    display: "flex",
+    justifyContent: "space-between",
+}
+const accountpar_style = {
+    marginLeft: "5.5%",
+    marginTop: "4.7%",
+    fontSize: ".8rem",
+    textDecoration: "underline",
+}
+const commentbtns_container = {
+    borderBottom: "1px solid grey",
+    borderTop: "1px solid",
+    marginTop: "1%",
+    padding: "2% 3%",
+    display: "flex",
+}
+const commentcancle_containerstyle = {
+    display: "flex",
+    justifyContent : "space-between",
+    alignItems : "center",
+    borderBottom : "1px solid #e1dbdb",
+
+}
+const commentsbtn_style = {
+    backgroundColor: "rgba(0,0,0,0)",
+    border: "none",
+    textTransform: "Capitalize",
+    fontSize: "1rem",
+    width: "50%",
+    paddingLeft: "2.5%",
+    textAlign : "left",
+}
+const commentdropdownbtn_style = {
+    backgroundColor: "rgba(0,0,0,0)",
+    border: "none",
+    width: "50%",
+    paddingLeft: "37%",
+}
+const commentcancle_parastyle = {
+    paddingLeft: "5%",
+    textTransform : "Capitalize",
+}
+const suggestioncontainer_style = {
+    backgroundColor: "rgb(61, 56, 56)"
+}
+
+
 
 const arts = {
-    img1 : {title : "The Duel", artist : "Loreal Milka" , price : "750$ ", img : img1 , boxstatus : [] , likestatus : []},
-    img2 : {title : "Familer Life", artist : "Eva Neewman" , price : "260$ " , img : img2 , boxstatus : [] , likestatus : []},
-    img3 : {title : "City Life Algarve,Portugal 1977", artist : "Nikoal Mulion" , price : "1100$ " ,img : img3 , boxstatus : [] , likestatus : []},
-    img4 : {title : "Fox Connect In", artist : "Bumia Lemia" , price : "1050$ " ,img : img4 , boxstatus :[] , likestatus : []},
-    img5 : {title : "The Particle Dao", artist : "Mitsu Aminkio" , price : "266$ ", img : img5 , boxstatus :[] , likestatus : []},
-    img6 : {title : "Waves of Hope", artist : "Humer Zimmermam" , price : "499$ ",img : img6 , boxstatus :[] , likestatus : []},
-
+    img1: {
+        account: "Lora Mikeal", title: "The Duel", artist: "Loreal Milka", price: "750$ ", img: img1, boxstatus: [], likestatus: ["white", "", "", 556],
+        comments: { chris: "Nice !!" }, commentsSize: 1
+    },
+    img2: {
+        account: "Kibrom Leul", title: "Familer Life", artist: "Eva Neewman", price: "260$ ", img: img2, boxstatus: [], likestatus: ["white", "", "", 29],
+        comments: { kevin: "", malcom: "very good price for a very good work ?", Leul: "Good price", malcomb: "Nice tecnique?" }, commentsSize: 4
+    },
+    img3: { account: "Mikel Gold", title: "City Life Algarve,Portugal 1977", artist: "Nikoal Mulion", price: "1100$ ", img: img3, boxstatus: [], likestatus: ["white", "", "", 1910], comments: { Natan: "wow !", NUNNNNNNNNN: "perfect" }, commentsSize: 2 },
+    img4: {
+        account: "Naten09", title: "Fox Connect In", artist: "Bumia Lemia", price: "1050$ ", img: img4, boxstatus: [], likestatus: ["white", "", "", 89],
+        comments: { murr: "Love this work. The price is too high tho..", malcomb: "why is this soo expensive ?" }, commentsSize: 2
+    },
+    img5: { account: "Anchelote--", title: "The Particle Dao", artist: "Mitsu Aminkio", price: "266$ ", img: img5, boxstatus: [], likestatus: ["white", "", "", 244], commentsSize: 0 },
+    img6: { account: "Pochetinio Polu", title: "Waves of Hope", artist: "Humer Zimmermam", price: "499$ ", img: img6, boxstatus: [], likestatus: ["white", "", "", 3601], commentsSize: 0 },
 }
 
